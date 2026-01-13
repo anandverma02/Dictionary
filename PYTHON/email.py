a3671d11a053b97f47604e4a539789765a4961ec
Anand Verma <addictiveanand02@gmail.com>
2026-01-13 21:38:34 +0530

    spotify homepage project is in html css folder

--- PYTHON/email.py
+++
@@ -1,6 +0,0 @@
-email='anandvermaa02@gmail.com'
-username,domain=email.split('@')
-if username.islower()==1 and username.isalnum()==1:
-    print('email is valid')
-else:
-    print('email is invalid')
